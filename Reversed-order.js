a7206871a56efa036173b31362035c9cd530999c
Nedelcho Dinev <ned.dinev@gmail.com>
2021-12-05 13:40:00 +0200

    Add files via upload

---
+++ Reversed-order.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 15;
+let c = 20;
+let arr = [];
+
+arr.push(a);
+arr.push(b);
+arr.push(c);
+
+for (let i = arr.length - 1; i >= 0; i--) {
+  console.log(arr[i]);
+}
+
+// or console.log(arr.reverse());
